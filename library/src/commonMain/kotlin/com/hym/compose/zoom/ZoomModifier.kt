55509c6c28df6ec01015063cb06ba4ab632df6d5
hehua2008 <hegan2010@gmail.com>
2024-04-04 15:51:50 +0800

    Fix the bug that the Composable width ZoomModifier can't be slid to others

--- library/src/commonMain/kotlin/com/hym/compose/zoom/ZoomModifier.kt
+++ library/src/commonMain/kotlin/com/hym/compose/zoom/ZoomModifier.kt
@@ -1,9 +1,15 @@
 package com.hym.compose.zoom
 
 import androidx.compose.animation.core.Animatable
+import androidx.compose.foundation.gestures.awaitEachGesture
+import androidx.compose.foundation.gestures.awaitFirstDown
+import androidx.compose.foundation.gestures.calculateCentroid
+import androidx.compose.foundation.gestures.calculateCentroidSize
+import androidx.compose.foundation.gestures.calculatePan
+import androidx.compose.foundation.gestures.calculateZoom
 import androidx.compose.foundation.gestures.detectTapGestures
-import androidx.compose.foundation.gestures.detectTransformGestures
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
@@ -17,9 +23,13 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.geometry.toRect
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.PointerInputScope
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.input.pointer.positionChanged
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.unit.toSize
+import androidx.compose.ui.util.fastAny
+import androidx.compose.ui.util.fastForEach
 import kotlinx.coroutines.launch
 import kotlin.math.abs
 
@@ -32,6 +42,7 @@ private const val TAG = "ZoomModifier"
 private const val MIN_ZOOM_SCALE = 1f
 private const val MAX_ZOOM_SCALE = 4f
 private const val DOUBLE_CLICK_ZOOM_SCALE = 2f
+private val DefaultFalseArray = { BooleanArray(4) { false } }
 
 /**
  * **NOTE**: Composable function modifiers are never skipped
@@ -58,11 +69,29 @@ fun Modifier.zoom(
     var panOffset by remember { mutableStateOf(Offset.Zero) }
     var zoomScale by remember { mutableFloatStateOf(1f) }
     val zoomAnimation = remember { Animatable(1f) }
+    val panRestriction by remember {
+        derivedStateOf {
+            val diff = layoutBounds.size * abs(zoomScale - 1)
+            Rect(-diff.width / 2, -diff.height / 2, diff.width / 2, diff.height / 2)
+        }
+    }
+    val leftTopRightBottomEdgeReached by remember {
+        derivedStateOf {
+            val curPanOffset = panOffset
+            val curPanRestriction = panRestriction
+            booleanArrayOf(
+                curPanOffset.x <= curPanRestriction.left,
+                curPanOffset.y <= curPanRestriction.top,
+                curPanOffset.x >= curPanRestriction.right,
+                curPanOffset.y >= curPanRestriction.bottom
+            )
+        }
+    }
     val scope = rememberCoroutineScope()
 
-    val onGesture: (centroid: Offset, pan: Offset, zoom: Float, rotation: Float, isGesture: Boolean) -> Unit =
+    val onGesture: (centroid: Offset, pan: Offset, zoom: Float, isGesture: Boolean) -> Unit =
         remember {
-            { centroid, pan, zoom, rotation, isGesture ->
+            { centroid, pan, zoom, isGesture ->
                 val boundsCenter = layoutBounds.center
                 val oldZoomScale = zoomScale
                 val beforeScaledCentroid = (centroid - boundsCenter) * oldZoomScale
@@ -77,18 +106,16 @@ fun Modifier.zoom(
                 val afterScaledCentroid = (centroid - boundsCenter) * newZoomScale
                 val adjustOffset = afterScaledCentroid - beforeScaledCentroid
 
-                val curLayoutBounds = layoutBounds
-                val maxOffsetX = abs(curLayoutBounds.width * (newZoomScale - 1)) / 2
-                val maxOffsetY = abs(curLayoutBounds.height * (newZoomScale - 1)) / 2
                 val oldPanOffset = panOffset
                 val scaledPanOffset = pan * newZoomScale
+                val curPanRestriction = panRestriction
                 val newPanOffset = Offset(
                     (oldPanOffset.x + scaledPanOffset.x - adjustOffset.x)
-                        .coerceAtLeast(-maxOffsetX)
-                        .coerceAtMost(maxOffsetX),
+                        .coerceAtLeast(curPanRestriction.left)
+                        .coerceAtMost(curPanRestriction.right),
                     (oldPanOffset.y + scaledPanOffset.y - adjustOffset.y)
-                        .coerceAtLeast(-maxOffsetY)
-                        .coerceAtMost(maxOffsetY)
+                        .coerceAtLeast(curPanRestriction.top)
+                        .coerceAtMost(curPanRestriction.bottom)
                 )
 
                 if (oldPanOffset != newPanOffset) {
@@ -104,7 +131,7 @@ fun Modifier.zoom(
             scope.launch {
                 zoomAnimation.snapTo(curScale)
                 zoomAnimation.animateTo(targetZoomScale) {
-                    onGesture(offset, Offset.Zero, value, 0f, false)
+                    onGesture(offset, Offset.Zero, value, false)
                 }
             }
         }
@@ -126,11 +153,80 @@ fun Modifier.zoom(
             translationY = curPanOffset.y
         }
         .pointerInput(onGesture) {
-            detectTransformGestures { centroid, pan, zoom, rotation ->
-                onGesture(centroid, pan, zoom, rotation, true)
+            detectZoomGestures(
+                leftTopRightBottomEdgeReached = { leftTopRightBottomEdgeReached.copyOf() },
+            ) { centroid, pan, zoom ->
+                onGesture(centroid, pan, zoom, true)
             }
         }
         .pointerInput(onClick, onLongClick, onDoubleTap) {
             detectTapGestures(onTap = onClick, onLongPress = onLongClick, onDoubleTap = onDoubleTap)
         }
+}
+
+suspend fun PointerInputScope.detectZoomGestures(
+    leftTopRightBottomEdgeReached: () -> BooleanArray = DefaultFalseArray,
+    onGesture: (centroid: Offset, pan: Offset, zoom: Float) -> Unit
+) {
+    awaitEachGesture {
+        var zoom = 1f
+        var pan = Offset.Zero
+        var pastTouchSlop = false
+        val touchSlop = viewConfiguration.touchSlop
+
+        awaitFirstDown(requireUnconsumed = false)
+        do {
+            val event = awaitPointerEvent()
+            val canceled = event.changes.fastAny { it.isConsumed }
+            if (!canceled) {
+                val zoomChange = event.calculateZoom()
+                val panChange = event.calculatePan()
+
+                if (!pastTouchSlop) {
+                    zoom *= zoomChange
+                    pan += panChange
+
+                    val centroidSize = event.calculateCentroidSize(useCurrent = false)
+                    val zoomMotion = abs(1 - zoom) * centroidSize
+                    val panMotion = pan.getDistance()
+
+                    if (zoomMotion > touchSlop ||
+                        panMotion > touchSlop
+                    ) {
+                        pastTouchSlop = true
+                    }
+                }
+
+                val edgeReached = leftTopRightBottomEdgeReached()
+                val cantPanX =
+                    (edgeReached[0] && panChange.x < 0) || (edgeReached[2] && panChange.x > 0)
+                val cantPanY =
+                    (edgeReached[1] && panChange.y < 0) || (edgeReached[3] && panChange.y > 0)
+                val absX = abs(panChange.x)
+                val absY = abs(panChange.y)
+
+                if (zoomChange <= 1f && ((cantPanX && absX > absY) || (cantPanY && absY > absX))) {
+                    // Not consume
+                } else {
+                    if (pastTouchSlop) {
+                        val centroid = event.calculateCentroid(useCurrent = false)
+                        val adjustedPanChange = Offset(
+                            if (cantPanX) 0f else panChange.x,
+                            if (cantPanY) 0f else panChange.y
+                        )
+                        if (zoomChange != 1f ||
+                            adjustedPanChange != Offset.Zero
+                        ) {
+                            onGesture(centroid, adjustedPanChange, zoomChange)
+                        }
+                    }
+                    event.changes.fastForEach {
+                        if (it.positionChanged()) {
+                            it.consume()
+                        }
+                    }
+                }
+            }
+        } while (!canceled && event.changes.fastAny { it.pressed })
+    }
 }
