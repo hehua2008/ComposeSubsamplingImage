12cff5d8a8e879ca2e9580df531b76ca18f26536
hehua2008 <hegan2010@gmail.com>
2024-04-22 10:19:06 +0800

    Use Alignment.Center to align contentBounds

--- library/src/commonMain/kotlin/com/hym/compose/utils/SizeExt.kt
+++ library/src/commonMain/kotlin/com/hym/compose/utils/SizeExt.kt
@@ -2,9 +2,15 @@ package com.hym.compose.utils
 
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.unit.IntSize
+import kotlin.math.roundToInt
 
 /**
  * @author hehua2008
  * @date 2024/4/3
  */
 fun Size.toOffset(): Offset = Offset(width, height)
+
+fun Size.roundToIntSize(): IntSize {
+    return IntSize(width.roundToInt(), height.roundToInt())
+}
