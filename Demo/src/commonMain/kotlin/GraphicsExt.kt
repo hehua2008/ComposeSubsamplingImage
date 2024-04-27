0e845c1311ba7ef2ed37fdf116b71ae6376599d9
hehua2008 <hegan2010@gmail.com>
2024-04-27 12:23:16 +0800

    Add Demo

---
+++ Demo/src/commonMain/kotlin/GraphicsExt.kt
@@ -0,0 +1,11 @@
+import androidx.compose.ui.graphics.ImageBitmap
+import coil3.annotation.ExperimentalCoilApi
+
+/**
+ * @author hehua2008
+ * @date 2024/4/26
+ */
+expect fun ByteArray.decodeToImageBitmap(): ImageBitmap
+
+@OptIn(ExperimentalCoilApi::class)
+expect fun coil3.Image.toImageBitmap(): ImageBitmap
