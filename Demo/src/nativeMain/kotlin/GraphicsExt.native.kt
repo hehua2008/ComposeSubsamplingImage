f0f8814094cb9864c472416cc5d31eb453152590
hehua2008 <hegan2010@gmail.com>
2024-05-17 11:24:14 +0800

    Use Coil to load images for ImageHorizontalPager

--- Demo/src/nativeMain/kotlin/GraphicsExt.native.kt
+++ Demo/src/nativeMain/kotlin/GraphicsExt.native.kt
@@ -7,6 +7,18 @@ import org.jetbrains.skia.impl.use
  * @author hehua2008
  * @date 2024/4/26
  */
+@OptIn(ExperimentalCoilApi::class)
+actual object EmptyCoilImage : coil3.Image {
+    override val size: Long = 0
+    override val width: Int = 0
+    override val height: Int = 0
+    override val shareable: Boolean = true
+
+    private val emptyBitmap = org.jetbrains.skia.Bitmap().apply { setImmutable() }
+
+    override fun toBitmap(): org.jetbrains.skia.Bitmap = emptyBitmap
+}
+
 actual fun ByteArray.decodeToImageBitmap(): ImageBitmap {
     org.jetbrains.skia.Image.makeFromEncoded(this).use { image ->
         val bitmap = org.jetbrains.skia.Bitmap.makeFromImage(image)
